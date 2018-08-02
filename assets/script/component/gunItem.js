e835a683d19c9bdec3980e6312206282008cab93
timegold-websrc <39736627+timegold-websrc@users.noreply.github.com>
2018-08-02 12:31:29 +0800

    v 1.0
    Complete

--- assets/script/component/gunItem.js
+++ assets/script/component/gunItem.js
@@ -18,22 +18,35 @@ cc.Class({
         
         this.namePosY = this.nameLabel.node.y;
         this.enabledStatus = false;
-
-
+        this.readySelect = false;
+        this.deltaTime = 0;
     },
 
     start () {
         this.node.on("touchend", function() {
-            this.drawSelected();
-            this.updateItemsPos();
-            var event = new cc.Event.EventCustom("selectGunItem", true);
-            event.setUserData(this.idx);
-            this.node.dispatchEvent(event);
+            if (this.enabledStatus) {
+                this.drawSelected();
+                this.updateItemsPos();
+                var event = new cc.Event.EventCustom("selectGunItem", true);
+                event.setUserData(this.idx);
+                this.node.dispatchEvent(event);
+            }
+        }, this);
+
+        this.node.on("touchstart", function() {
+            if (this.readySelect) {
+                this.readySelect = false;
+                this.enabledStatus = true;
+                this.setItemEnable(true);
+                var event = new cc.Event.EventCustom("payGunItem", true);
+                event.setUserData(this.idx);
+                this.node.dispatchEvent(event);    
+            }
         }, this);
     },
 
-    setItem(gun, idx, status) {
-        
+    //. 현재 총의 Item에 대한 설정을 진행한다.
+    setItem(gun, idx, status) {        
         this.idx = idx;
 
         this.gun = cc.instantiate(gun);
@@ -49,6 +62,7 @@ cc.Class({
         this.detailLabel.node.active = false;
         this.drawUnSelect(enable);
         this.setItemEnable(enable);
+        this.enabledStatus = enable;
     },
 
     setItemEnable(enable) {        
@@ -60,20 +74,15 @@ cc.Class({
         this.nameLabel.node.color = w_color;
         this.detailLabel.node.color = w_color;
 
-        this.enabledStatus = enable;
+        
     },
     drawUnSelect(active) {
         var g = this.bgGraphics;
         var x = -80;
         var w = 160;
         g.clear();
-        if (active) {
-            g.fillColor = this.selColor;
-            g.lineWidth = 5;
-        } else {
-            g.fillColor = cc.color(0, 0, 0, 100);
-            g.lineWidth = 0;
-        }
+        g.fillColor = cc.color(0, 0, 0, 100);
+        g.lineWidth = 0;
         g.roundRect(x, x, w, w, 5);
         g.fill();
         g.stroke();
@@ -97,7 +106,7 @@ cc.Class({
         this.detailLabel.node.active = true;
         this.gun.y = 30;
         this.gun.setScale(1.4);
-        
+      
     },
     initItemPos() {
         this.nameLabel.node.y = this.namePosY;
@@ -106,7 +115,49 @@ cc.Class({
         this.detailLabel.node.active = false;
     },
 
-    
+    //. 총의 선택상태를 체크해준다.
+    readySelectGun() {
+        if (!this.enabledStatus) {
+            console.log('readySelect:' + this.idx);
+            this.readySelect = true;
+            this.deltaTime = (this.idx % 6) / 10;
+            this.setItemEnable(true); 
+        }
+    },    
+    //. 총의 선태상태를 해제해준다.
+    stopSelectGun() {
+        if (!this.enabledStatus) {
+            console.log("stop ready:" + this.idx);
+            this.readySelect = false;
+            this.drawUnSelect(false);
+            this.setItemEnable(false);
+        }
+    },
+    // 총이 선택상태이면 총의 배경을 바꾸어준다.
+    update (dt) {
+        if (this.readySelect) {
+            this.deltaTime += dt;
+            if (this.deltaTime > 0.3) {
+                this.deltaTime = 0;
+                this.drawBGRandon();
+            }
+        }
+    },
+    //. 우연적으로 배경을 바꾸어준다.
+    drawBGRandon() {
+        var t = 100;
+        var c1 = Math.ceil(cc.random0To1() * t + t);
+        var c2 = Math.ceil(cc.random0To1() * t + t);
+        var c3 = Math.ceil(cc.random0To1() * t + t);
 
-    // update (dt) {},
+        var g = this.bgGraphics;
+        var x = -80;
+        var w = 160;
+        g.clear();
+        g.fillColor = cc.color(c1, c2, c3, 255);
+        g.lineWidth = 0;
+        g.roundRect(x, x, w, w, 5);
+        g.fill();
+        g.close();
+    },
 });
--- assets/script/gunListLayout.js
+++ assets/script/gunListLayout.js
@@ -11,7 +11,9 @@ cc.Class({
         gameMain: cc.Node,
         startGameLayout: cc.Layout,
         backBtn: cc.Node,
-        pageViewContainer: cc.PageView
+        gunPageView: cc.PageView,
+        coinCntLabel: cc.Label,
+        buyGunBtn: cc.Node
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -20,10 +22,20 @@ cc.Class({
         this.gamePlayerComp = this.gameMain.getComponent('bgMap').player.getComponent('player');
         this.gunItmeObjs = [];
         this.selectedIdx = 0;
+        this.payCoinCnt = 250;
+        this.coinCntLabel.string = this.payCoinCnt;
+        this.nowPageIdx = 0;
+        this.gunSelectedIdx = [];
+        this.selectedIdx = parseInt(cc.sys.localStorage.getItem("gun_num"));
+        this.payStatus = false;
+
     },
 
     start () {
         this.createItem();
+
+        //. 해당한 총을 선택하였을때의 사건이다.
+        //. 총을 선택하면 이미 선택되였던 총은 비선택상태로 놓고 현재의 총을 선택상태로 놓은 다음 총의 시험을 진행한다.
         this.node.on('selectGunItem', function(event) {
             var idx = event.getUserData();
             if (idx != this.selectedIdx) {                
@@ -37,13 +49,47 @@ cc.Class({
 
         }, this);
 
+        //. 비활성화된 총을 선택하였을때의 사건
+        //. 총을 선택하면 활성화 상태로 놓고 총의 id를 1로 설정하고 보관한다.
+        this.node.on('payGunItem', function(event) {
+            var idx = event.getUserData();
+            this.gunSelectedIdx[idx] = 1;
+            cc.sys.localStorage.setItem('enableGunList', this.gunSelectedIdx);
+            this.stopSelectGun();
+            this.payStatus = false;
+        }, this);
+
         this.backBtn.on('touchstart', function(){
             this.node.active = false;
         }, this);
 
-        this.pageViewContainer.node.on('page-turning', function() {
-            console.log('NextPage');
-        });
+        //. 현재 페지의 사건.
+        //. 페지 이동에 따라 총의 가격이 달라진다.
+        this.gunPageView.node.on('page-turning', function() {
+            var idx = this.gunPageView.getCurrentPageIndex();
+
+            if (idx == 0) {
+                this.payCoinCnt = 250;
+            } else if (idx == 1 || idx == 2) {
+                this.payCoinCnt = 500;
+            } else {
+                this.payCoinCnt = 750;
+            }
+            this.coinCntLabel.string = this.payCoinCnt;
+        }, this);
+
+        //. 총얻기 단추를 누룰때의 사건이다.
+        this.buyGunBtn.on('touchstart', function() {
+            if (this.payStatus) return;
+                
+            var coinCnt = parseInt(cc.sys.localStorage.getItem("coinCount"));
+            if (coinCnt < this.payCoinCnt) return;
+            var payStatus = this.startSelectGun();
+            if (payStatus) {
+                this.payStatus = true;
+                this.gameMain.getComponent('bgMap').setCoinCount(this.payCoinCnt - coinCnt);
+            }
+        }, this);
     },
 
     
@@ -59,7 +105,9 @@ cc.Class({
         this.startGameLayout.node.active = true;
     },
 
+    //. 총의 목록을 구성한다.
     createItem() {
+        
         var gunCnt = [6, 6, 5, 6];
         var step = 40;
         var guns = this.gamePlayerComp.gunPrefab;
@@ -67,22 +115,29 @@ cc.Class({
 
         var gunIdx = [];
         var tmp = cc.sys.localStorage.getItem("enableGunList");
+        //. 총의 활성상태를 얻어온다.
         if (!Array.isArray(tmp)) {
             gunIdx = tmp.split(",");
         } else {
             gunIdx = tmp;
         }
-
         for (var i = 0; i < gunIdx.length; i++) {
             gunIdx[i] = parseInt(gunIdx[i]);
-        }
-        
+        }        
+        this.gunSelectedIdx = gunIdx;
+
         for (var i = 0; i < gunCnt.length; i++) {
             var row = 0, col = 0;
             for (var j = 0; j < gunCnt[i]; j++) {
                 var item = cc.instantiate(this.gunItem);
                 this.pages[i].addChild(item);
                 item.getComponent('gunItem').setItem(guns[idx], idx, gunIdx[idx]);
+
+                //. 현재 선택된 총이면 선택상태를 체크해준다.
+                if (idx == this.selectedIdx) {
+                    item.getComponent('gunItem').drawSelected();
+                    item.getComponent('gunItem').updateItemsPos();
+                }
 
                 if (row > 2) {
                     row = 0; col++;
@@ -101,6 +156,28 @@ cc.Class({
     saveSelectedGun(idx) {
         var ls = cc.sys.localStorage;
         ls.setItem("gun_num", idx);
+    },
+
+    //. 총의 선택이 시작되였을때의 처리.
+    startSelectGun() {
+        var startIdx = [0, 6, 12, 17];
+        var gunCnt   = [6, 6,  5, 6];
+        var existGun = false;
+        var idx = this.gunPageView.getCurrentPageIndex();
+        for (var i = startIdx[idx]; i < startIdx[idx] + gunCnt[idx]; i++) {
+            this.gunItmeObjs[i].getComponent('gunItem').readySelectGun();
+            this.existGun = true;
+        }
+        return existGun;
+    },
+    //. 총의 선택이 끝낱을때의 처리이다.
+    stopSelectGun() {
+        var startIdx = [0, 6, 12, 17];
+        var gunCnt   = [6, 6,  5, 6];
+        var idx = this.gunPageView.getCurrentPageIndex();
+        for (var i = startIdx[idx]; i < startIdx[idx] + gunCnt[idx]; i++) {
+            this.gunItmeObjs[i].getComponent('gunItem').stopSelectGun();
+        }
     }
 
     // update (dt) {},
